1a6fdaab60d5d9410caaf2f02481c1c78cad7eac
-T.K.- <tk_dandelion@126.com>
2018-06-02 08:19:12 +0800

    error corrected

--- azrec_tflearn_official.py
+++ azrec_tflearn_official.py
@@ -4,14 +4,25 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-train_file = "data/a_z_handwritten.npy"
+
+load_from_model = True
+
+train_file = 'data/a_z_handwritten.npy'
 
 num_classes = 26
 
 def load_data():
-    raw = np.load(train_file)
+    try:
+        raw = np.load(train_file)
+    except FileNotFoundError as e:
+        print('No such file called %s' % train_file)
+        print('Loading from csv...')
+        raw = np.loadtxt(train_file.replace('npy', 'csv'), delimiter=',')
+        # raw[:, 1:785] = raw[:, 1:785] / 255
+        np.save(train_file, raw)
     np.random.shuffle(raw)
     y_label = raw[:, 0]
+    print(y_label)
     out_y = keras.utils.to_categorical(y_label, num_classes)
     num_images = raw.shape[0]
     out_x = raw[:, 1:785]
@@ -19,14 +30,15 @@ def load_data():
 
 
 X, Y = load_data()
-
-X, Y = X[:100000], Y[:100000]
 testX, testY = X[-1000:], Y[-1000:]
+print(Y)
+X, Y = X[:80000], Y[:80000]
+
 # Building deep neural network
 net = tflearn.input_data(shape=[None, 784])                                                       # input_layer
 net = tflearn.fully_connected(net, 1024, activation='relu', regularizer='L2', weight_decay=0.001) # dense1
 net = tflearn.dropout(net, 0.9)                                                                   # dropout1
-net = tflearn.fully_connected(net, 256, activation='relu', regularizer='L2', weight_decay=0.001)  # dense2
+net = tflearn.fully_connected(net, 512, activation='relu', regularizer='L2', weight_decay=0.001)  # dense2
 net = tflearn.dropout(net, 0.9)                                                                   # dropout2
 net = tflearn.fully_connected(net, 128, activation='relu', regularizer='L2', weight_decay=0.001)  # dense3
 net = tflearn.dropout(net, 0.9)                                                                   # dropout3
@@ -40,24 +52,28 @@ net = tflearn.regression(softmax, optimizer=sgd, metric=tflearn.metrics.Accuracy
 
 # Training
 model = tflearn.DNN(net, tensorboard_verbose=0)
+'''
 model.fit(X, Y, n_epoch=4, validation_set=(testX, testY),
           show_metric=True, run_id="dense_model")
 
 model.save('model.tfl')
+'''
 
-preds = model.predict(testX)
-for i, pred in enumerate(preds):
-    print('--- No.%d out of %d ---' % (i + 1, len(preds)))
-    pos = high_n = 0
-    for m, n in enumerate(testY[i]):
-        if n > high_n:
-            pos, high_n = m, n
-    print('Actual: ', chr(65 + pos))
-    pos = high_n = 0
-    for m, n in enumerate(pred):
-        if n > high_n:
-            pos, high_n = m, n
-    print('Predict: ', chr(65 + pos))
-    plt.imshow(testX[i].reshape(28, 28) * 255, cmap=plt.cm.gray)
-    plt.pause(0.5)
-    plt.close()
+model.load('model/model.tfl')
+
+preds = model.predict([testX[2]])
+print(preds)
+pos = n_hi = 0
+print(testY[2])
+for i, n in enumerate(testY[2]):
+	if i > n_hi:
+		pos, n_hi = i, n
+print(chr(pos+65))
+pos = n_hi = 0
+for i, n in enumerate(preds[0]):
+	if i > n_hi:
+		pos, n_hi = i, n
+print(chr(pos+65))
+plt.imshow(testX[2].reshape(28, 28), cmap=plt.cm.gray)
+plt.pause(2)
+plt.close()
